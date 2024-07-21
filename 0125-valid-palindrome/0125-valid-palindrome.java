777452a64992a0e6accf432f238a71e499da6f5f
Sid541 <144512053+Sid541@users.noreply.github.com>
2024-07-22 01:10:51 +0530

    Time: 14 ms (34.54%), Space: 45.5 MB (15.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,9 +4,6 @@ class Solution {
         String str=s1.replaceAll("[^a-zA-Z0-9]","");
         int i=0;
         int j=str.length()-1;
-        if(s1==" "){
-            return true;
-        }
         while(i<=j){
             if(str.charAt(i)!=str.charAt(j)){
                 return false;
