b98dfd737b0262072bf0746718adee14592283c0
Sid541 <144512053+Sid541@users.noreply.github.com>
2024-08-02 02:33:54 +0530

    Update 0050-powx-n.java

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -5,22 +5,19 @@ class Solution {
         if (n == 0) {
             return 1;
         }
-
         
         long N = n;
-
         
         if (N < 0) {
             N = -N;
             x = 1 / x;
         }
 
-        
         if (N % 2 == 0) {
             return myPow(x * x, (int) (N / 2));
         } 
     
-        // If N is odd, recursively compute x^(N-1) and multiply it by x
+        
         else {
             return x * myPow(x, (int) (N - 1));
         }
