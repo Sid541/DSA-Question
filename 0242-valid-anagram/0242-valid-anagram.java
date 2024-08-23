304c507d0289d54ecb38aa44160eb5e3fd813b8e
Sid541 <144512053+Sid541@users.noreply.github.com>
2024-08-23 13:44:06 +0530

    Time: 16 ms (23.14%), Space: 44.7 MB (26.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+       /* if(s.length()!=t.length()){
             return false;
         }
         
@@ -13,10 +13,40 @@ class Solution {
                     break;
                 }
         }
-        if(flag==0){
+             if(flag==0){
+                return false;
+             }
+            }
+        return true;*/
+
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        if(s.length()!=t.length()){
             return false;
         }
+
+        for(char c: s.toCharArray()){
+            if(count.containsKey(c)){
+                count.put(c,count.get(c)+1);
             }
+            else{
+                count.put(c,1);
+            }
+        }
+
+        for(char c: t.toCharArray()){
+            if(count.containsKey(c)){
+                count.put(c,count.get(c)-1);
+            }
+            else{
+                count.put(c,0);
+            }
+        }
+        for(int val : count.values()){
+            if(val!=0){
+                return false;
+            }
+        }
         return true;
         
     }
